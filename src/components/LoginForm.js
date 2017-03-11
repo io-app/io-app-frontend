4ddf6b8ee6ebf7910b44e3e6f196562934e41ee6
David Herrmann <davherrmann@googlemail.com>
2017-03-11 19:02:13 +0000

    remove unused imports

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card'
+import {Card, CardActions, CardTitle} from 'material-ui/Card'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 
