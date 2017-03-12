cfee84ae4581b32371f60bbdbee275419f7430ba
David Herrmann <davherrmann@googlemail.com>
2017-03-12 21:40:07 +0000

    add possibility to toggle the drawer

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import AppBar from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton'
 import {List, ListItem} from 'material-ui/List'
@@ -25,53 +25,70 @@ const styles = {
   }
 }
 
-export default props =>
-  <div style={styles.main}>
-    <AppBar
-      title='Dashboard'
-      iconElementRight={<FlatButton label='Save' />}
-    />
-    <div style={styles.content}>
-      <List style={styles.drawer}>
-        <ListItem primaryText='Receipts' leftIcon={<ContentInbox />} />
-        <ListItem primaryText='Starred' leftIcon={<ActionGrade />} />
-        <ListItem primaryText='Drafts' leftIcon={<ContentDrafts />} />
-      </List>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHeaderColumn>ID</TableHeaderColumn>
-            <TableHeaderColumn>Category</TableHeaderColumn>
-            <TableHeaderColumn>Amount</TableHeaderColumn>
-            <TableHeaderColumn>Date</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableRowColumn>1</TableRowColumn>
-            <TableRowColumn>Project A</TableRowColumn>
-            <TableRowColumn><b>€2310.10</b></TableRowColumn>
-            <TableRowColumn>10/10/2010</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>2</TableRowColumn>
-            <TableRowColumn>Project B</TableRowColumn>
-            <TableRowColumn><b>$1029</b></TableRowColumn>
-            <TableRowColumn>15/10/2010</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>3</TableRowColumn>
-            <TableRowColumn>Worker Steven Sanders</TableRowColumn>
-            <TableRowColumn><b>€100</b></TableRowColumn>
-            <TableRowColumn>18/10/2010</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>4</TableRowColumn>
-            <TableRowColumn>Worker John Smith</TableRowColumn>
-            <TableRowColumn><b>€1201</b></TableRowColumn>
-            <TableRowColumn>28/10/2010</TableRowColumn>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
-  </div>
+class Dashboard extends Component {
+  toggleDrawer = () => this.setState({drawerOpen: !this.state.drawerOpen})
+
+  state = {
+    drawerOpen: true
+  }
+
+  render () {
+    return (
+      <div style={styles.main}>
+        <AppBar
+          title='Dashboard'
+          iconElementRight={<FlatButton label='Save' />}
+          onLeftIconButtonTouchTap={this.toggleDrawer}
+        />
+        <div style={styles.content}>
+          {this.state.drawerOpen
+            ? <List style={styles.drawer}>
+              <ListItem primaryText='Receipts' leftIcon={<ContentInbox />} />
+              <ListItem primaryText='Starred' leftIcon={<ActionGrade />} />
+              <ListItem primaryText='Drafts' leftIcon={<ContentDrafts />} />
+            </List>
+            : null
+          }
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHeaderColumn>ID</TableHeaderColumn>
+                <TableHeaderColumn>Category</TableHeaderColumn>
+                <TableHeaderColumn>Amount</TableHeaderColumn>
+                <TableHeaderColumn>Date</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableRowColumn>1</TableRowColumn>
+                <TableRowColumn>Project A</TableRowColumn>
+                <TableRowColumn><b>€2310.10</b></TableRowColumn>
+                <TableRowColumn>10/10/2010</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>2</TableRowColumn>
+                <TableRowColumn>Project B</TableRowColumn>
+                <TableRowColumn><b>$1029</b></TableRowColumn>
+                <TableRowColumn>15/10/2010</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>3</TableRowColumn>
+                <TableRowColumn>Worker Steven Sanders</TableRowColumn>
+                <TableRowColumn><b>€100</b></TableRowColumn>
+                <TableRowColumn>18/10/2010</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>4</TableRowColumn>
+                <TableRowColumn>Worker John Smith</TableRowColumn>
+                <TableRowColumn><b>€1201</b></TableRowColumn>
+                <TableRowColumn>28/10/2010</TableRowColumn>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
