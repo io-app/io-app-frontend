657f45230536ff5e8d8029a381bf4c6bbdec9a32
David Herrmann <davherrmann@googlemail.com>
2017-03-12 21:04:28 +0000

    add drawer list to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton'
+import {List, ListItem} from 'material-ui/List'
+import ContentInbox from 'material-ui/svg-icons/content/inbox'
+import ActionGrade from 'material-ui/svg-icons/action/grade'
+import ContentDrafts from 'material-ui/svg-icons/content/drafts'
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table'
-import { cyan500 } from 'material-ui/styles/colors'
 
 const styles = {
   main: {
@@ -10,7 +13,15 @@ const styles = {
     flexDirection: 'column',
     minHeight: '100vh',
     alignItems: 'center',
-    backgroundColor: cyan500
+    backgroundColor: '#fff'
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  drawer: {
+    minWidth: '200px',
+    paddingRight: '20px'
   }
 }
 
@@ -20,40 +31,47 @@ export default props =>
       title='Dashboard'
       iconElementRight={<FlatButton label='Save' />}
     />
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHeaderColumn>ID</TableHeaderColumn>
-          <TableHeaderColumn>Category</TableHeaderColumn>
-          <TableHeaderColumn>Amount</TableHeaderColumn>
-          <TableHeaderColumn>Date</TableHeaderColumn>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableRowColumn>1</TableRowColumn>
-          <TableRowColumn>Project A</TableRowColumn>
-          <TableRowColumn><b>€2310.10</b></TableRowColumn>
-          <TableRowColumn>10/10/2010</TableRowColumn>
-        </TableRow>
-        <TableRow>
-          <TableRowColumn>2</TableRowColumn>
-          <TableRowColumn>Project B</TableRowColumn>
-          <TableRowColumn><b>$1029</b></TableRowColumn>
-          <TableRowColumn>15/10/2010</TableRowColumn>
-        </TableRow>
-        <TableRow>
-          <TableRowColumn>3</TableRowColumn>
-          <TableRowColumn>Worker Steven Sanders</TableRowColumn>
-          <TableRowColumn><b>€100</b></TableRowColumn>
-          <TableRowColumn>18/10/2010</TableRowColumn>
-        </TableRow>
-        <TableRow>
-          <TableRowColumn>4</TableRowColumn>
-          <TableRowColumn>Worker John Smith</TableRowColumn>
-          <TableRowColumn><b>€1201</b></TableRowColumn>
-          <TableRowColumn>28/10/2010</TableRowColumn>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <div style={styles.content}>
+      <List style={styles.drawer}>
+        <ListItem primaryText='Receipts' leftIcon={<ContentInbox />} />
+        <ListItem primaryText='Starred' leftIcon={<ActionGrade />} />
+        <ListItem primaryText='Drafts' leftIcon={<ContentDrafts />} />
+      </List>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderColumn>ID</TableHeaderColumn>
+            <TableHeaderColumn>Category</TableHeaderColumn>
+            <TableHeaderColumn>Amount</TableHeaderColumn>
+            <TableHeaderColumn>Date</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableRowColumn>1</TableRowColumn>
+            <TableRowColumn>Project A</TableRowColumn>
+            <TableRowColumn><b>€2310.10</b></TableRowColumn>
+            <TableRowColumn>10/10/2010</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>2</TableRowColumn>
+            <TableRowColumn>Project B</TableRowColumn>
+            <TableRowColumn><b>$1029</b></TableRowColumn>
+            <TableRowColumn>15/10/2010</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>3</TableRowColumn>
+            <TableRowColumn>Worker Steven Sanders</TableRowColumn>
+            <TableRowColumn><b>€100</b></TableRowColumn>
+            <TableRowColumn>18/10/2010</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>4</TableRowColumn>
+            <TableRowColumn>Worker John Smith</TableRowColumn>
+            <TableRowColumn><b>€1201</b></TableRowColumn>
+            <TableRowColumn>28/10/2010</TableRowColumn>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   </div>
